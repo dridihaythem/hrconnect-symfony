5f240ddcc11285fe1f0dd7c60f7c778e2c6827c7
Adam <adam.chebaane@esprit.tn>
2025-04-08 05:55:28 +0100

    reclamation

---
+++ src/Form/ReclamationType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reclamation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReclamationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('employeeName', TextType::class, [
+                'label' => 'Nom de l\'employé',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('type', ChoiceType::class, [
+                'label' => 'Type de réclamation',
+                'choices' => [
+                    'Harcèlement au travail' => 'Workplace Harassment',
+                    'Problème de salaire' => 'Salary Issue',
+                    'Conditions de travail' => 'Working Conditions',
+                    'Autre' => 'Other',
+                ],
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'class' => 'form-control',
+                    'rows' => 5
+                ]
+            ])
+            ->add('priority', ChoiceType::class, [
+                'label' => 'Priorité',
+                'choices' => [
+                    'Faible' => 'Low',
+                    'Moyenne' => 'Medium',
+                    'Élevée' => 'High',
+                ],
+                'attr' => ['class' => 'form-control']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Reclamation::class,
+        ]);
+    }
+}
